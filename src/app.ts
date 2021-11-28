b78b3971f43c699762c757e44c916d9c686328fc
Adil Echcharafi <charafi.adil@gmail.com>
2021-11-28 14:15:39 +0100

    One

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import lusca from 'lusca'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 import movieRouter from './routers/movie'
 import bookRouter from './routers/book'
@@ -10,18 +11,37 @@ import authorRouter from './routers/author'
 import apiErrorHandler from './middlewares/apiErrorHandler'
 import apiContentType from './middlewares/apiContentType'
 import compression from 'compression'
+import bodyParser from 'body-parser'
 
 dotenv.config({ path: '.env' })
 const app = express()
 
 // Express configuration
 app.set('port', process.env.PORT || 3000)
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(
+  cors({
+    methods: '*',
+    origin: process.env.CORS_CONFIG_ORIGIN?.split(';') ?? [],
+    allowedHeaders: '*',
+  })
+)
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit: 50000,
+  })
+)
+app.use(bodyParser.json())
 app.use(apiContentType)
 // Use common 3rd-party middlewares
 app.use(compression())
-app.use(express.json())
-app.use(lusca.xframe('SAMEORIGIN'))
+//app.use(lusca.xframe('SAMEORIGIN'))
 app.use(lusca.xssProtection(true))
+
+// Custom API error handler
+app.use(apiErrorHandler)
 
 // Use movie router
 app.use('/api/v1/movies', movieRouter)
@@ -29,8 +49,5 @@ app.use('/api/v1/books', bookRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/userBooks', userBooksRouter)
 app.use('/api/v1/authors', authorRouter)
-
-// Custom API error handler
-app.use(apiErrorHandler)
 
 export default app
